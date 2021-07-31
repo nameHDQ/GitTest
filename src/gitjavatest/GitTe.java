aeb61a83d9cf66d0f29412961c3f6c591443ab4f
hdq <1758398270@qq.com>
2021-07-31 12:23:59 +0800

    second commit

--- src/gitjavatest/GitTe.java
+++ src/gitjavatest/GitTe.java
@@ -7,5 +7,6 @@ package gitjavatest;
 public class GitTe {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
