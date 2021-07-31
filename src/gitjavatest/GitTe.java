c3c18fa1a92ec538420f717d3fa08021e3e6c35b
hdq <1758398270@qq.com>
2021-07-31 12:17:09 +0800

    first commit

---
+++ src/gitjavatest/GitTe.java
@@ -0,0 +1,11 @@
+package gitjavatest;
+
+/**
+ * @author hdq
+ * @create 2021-07-31 12:07
+ */
+public class GitTe {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
